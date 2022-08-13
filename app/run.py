8d677a58b72ab0fb1cec203f616e3e7f762d90a3
rodrigo.silva.cunha <rodrigo.silva.cunha@g.globo>
2022-08-13 17:36:21 -0300

    isX

--- app/run.py
+++ app/run.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__, ...)
+app = Flask(__name__,...)
 
 @app.route("/")
 def home():
     return "Hello, World!"
 
-if 'run' == "__main__":
+if __name__ == "__main__":
     app.run(debug=True)
